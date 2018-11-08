d52199ac07541d763070e933330403a1900c75fb
acevedoj <acevedoj@uw.edu>
2018-11-07 23:02:43 -0800

    finished lab

--- app/src/main/java/edu/uw/memorylab/BirdActivity.java
+++ app/src/main/java/edu/uw/memorylab/BirdActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 public class BirdActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,4 +31,20 @@ public class BirdActivity extends AppCompatActivity {
         birdView.setImageDrawable(image);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ImageView birdView = (ImageView)findViewById(R.id.imgBird);
+        birdView.setImageDrawable(null);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Drawable image = ContextCompat.getDrawable(this, R.drawable.hummingbird); //get the drawable resource
+        ImageView birdView = (ImageView)findViewById(R.id.imgBird);
+
+        birdView = (ImageView)findViewById(R.id.imgBird);
+        birdView.setImageDrawable(image);
+    }
 }
